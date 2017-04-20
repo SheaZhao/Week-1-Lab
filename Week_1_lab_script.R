f3dc49f8a591cf8358a3f71d6f8e89860335bf6c
Shea Zhao <zhaoshea@gmail.com>
2017-04-19 18:59:43 -0700

    initial commit.

---
+++ Week_1_lab_script.R
@@ -0,0 +1,31 @@
+## Week 1 Lab Assignment ####
+
+# 1.) Write a code that swaps variables.
+
+cat <- 'meow'
+dog <- 'bark'
+
+swap <- cat
+swap # meow
+
+cat <- dog
+cat #bark
+
+dog <- swap
+dog #meow
+
+print(cat)
+print(dog)
+
+
+
+# 2.) write a code in R which given your first and last name prints your full name.
+
+first <- 'Shea'
+last <- 'Zhao'
+
+paste(first, last)
+paste(first, last, sep = '')  #no separation
+
+
+
